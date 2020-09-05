4bf523ee2a9f4dc48fee7c06c0f508f3668d24d3
Jose Garzon <jd.garzon12@gmail.com>
2020-09-05 18:00:06 -0500

    Updates

---
+++ precache-manifest.9191b94e79bd1683e14d81e871c5e9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c1eaec31f7079ab48b17bd64c006a3",
+    "url": "/platzi-100-coding-challenge/index.html"
+  },
+  {
+    "revision": "5e3510a98c2d1754307d",
+    "url": "/platzi-100-coding-challenge/static/css/main.21da30c6.chunk.css"
+  },
+  {
+    "revision": "b1e5fb96da9a05b7c4b2",
+    "url": "/platzi-100-coding-challenge/static/js/2.98a0c782.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/platzi-100-coding-challenge/static/js/2.98a0c782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3510a98c2d1754307d",
+    "url": "/platzi-100-coding-challenge/static/js/main.68d2fb12.chunk.js"
+  },
+  {
+    "revision": "85a20a101a036aa0baab",
+    "url": "/platzi-100-coding-challenge/static/js/runtime-main.c94b1544.js"
+  }
+]);
--- src/layout/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo = () => {
-  return <div className="logo">100 Days of Coding</div>;
-};
-
-export default Logo;
